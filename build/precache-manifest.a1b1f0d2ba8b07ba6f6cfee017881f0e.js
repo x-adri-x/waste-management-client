de835daedf74eb92079f9e0fd4df689a5bad19b2
Lenart Adrienn <adrienn@adrienn.sspinc.io>
2021-02-06 13:38:25 +0100

    first commit

---
+++ build/precache-manifest.a1b1f0d2ba8b07ba6f6cfee017881f0e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21ba53574dfe98a4c9e6b9fb2d1428e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97385fa0935874b4ea49",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "31994aec61bae943326c",
+    "url": "/static/css/main.cca16f73.chunk.css"
+  },
+  {
+    "revision": "97385fa0935874b4ea49",
+    "url": "/static/js/2.2291f709.chunk.js"
+  },
+  {
+    "revision": "0d68d4392bba0a65a7fa8c172929670f",
+    "url": "/static/js/2.2291f709.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31994aec61bae943326c",
+    "url": "/static/js/main.6f84062b.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "7083676a734cc1468def32d0c85cbe16",
+    "url": "/static/media/avatar.7083676a.jpg"
+  }
+]);
