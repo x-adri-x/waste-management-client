e253a6c323116a8ea0278f2eef6865fc0b60ddde
Lenart Adrienn <adrienn@adrienn.sspinc.io>
2021-04-26 13:20:28 +0200

    final_01

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,8 +3,8 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
-import {listState, authState} from '../atoms/Atoms.js'
-import {useRecoilState, useSetRecoilState} from 'recoil'
+import {listState, authState, messageState} from '../atoms/Atoms.js'
+import {useSetRecoilState, useRecoilState} from 'recoil'
 import Messages from './Messages.js'
 import List from '../components/List.js'
 import {toggleHidden} from '../utils/toggleHidden.js'
@@ -13,7 +13,8 @@ import {formatDriver} from '../utils/formatting.js'
 
 function Home () {
     const setList = useSetRecoilState(listState)
-    const [auth, setAuth] = useRecoilState(authState)
+    const setAuth = useSetRecoilState(authState)
+    const [message, setMessage] = useRecoilState(messageState)
 
     const showMessagesOpts = () => {
         toggleHidden('messagesStarter')
@@ -39,6 +40,26 @@ function Home () {
       toggleHidden('list')
     }
 
+    const generateRoute = async() => {
+      setList([])
+      toggleHidden('route')
+      let searchParam = {
+        "status_id": 1
+      }
+      let url = generateUrl('drivers/search')
+      let result = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(searchParam)
+          })
+          .then(response => response.json())
+          .then(result => result)
+          console.log(result)
+          result.length > 0 ? setMessage(`Currently ${result.length} driver/s are available.`) : setMessage('There are no logged in drivers.') 
+    }
 
     const logout = () => {
       setAuth(false)
@@ -53,12 +74,14 @@ function Home () {
               <Col className = 'drivers-admin-btns'>
                 <Button variant = 'light' size = 'large' onClick = {showMessagesOpts}>messages</Button>
                 <Button variant = 'light' size = 'large' onClick = {showLoggedInDrivers}>available drivers</Button>
+                <Button variant = 'light' size = 'large' onClick = {generateRoute}>send route</Button>
                 <Button variant = 'light' size = 'large' onClick = {logout} className = 'logout'>logout</Button>
               </Col>
               <Col></Col>
             </Row>
           </Col>
           <Col className = 'col-drivers-data-tab'>
+            <p className = 'route hidden'>{message}</p>
             <Messages />
             <List />
           </Col>
